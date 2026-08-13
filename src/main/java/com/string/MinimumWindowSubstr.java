6895c17e63873d07b0a8c37ad65c85f398348898
saurabhprakash <saurabhprakash0504@gmail.com>
2026-08-13 17:45:55 +0100

    feat: implement MinimumWindowSubstr class with minWindow method for minimum window substring search

---
+++ src/main/java/com/string/MinimumWindowSubstr.java
@@ -0,0 +1,47 @@
+package com.string;
+
+import java.util.*;
+
+public class MinimumWindowSubstr {
+
+    //YT - Take U forward
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        MinimumWindowSubstr obj = new MinimumWindowSubstr();
+        System.out.println(obj.minWindow(s, t));
+    }
+
+    public String minWindow(String s, String t) {
+        int l = 0;
+        int r = 0;
+        HashMap<Character, Integer> mpp = new HashMap<>();
+        int cnt = 0;
+        int sindex = -1;
+        int minlen = Integer.MAX_VALUE;
+
+        for (int i = 0; i < t.length(); i++) {
+            mpp.put(t.charAt(i), mpp.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        while (r < s.length()) {
+            char ch = s.charAt(r);
+            if (mpp.containsKey(ch) && mpp.get(ch) > 0) {
+                cnt++;
+            }
+            mpp.put(ch, mpp.getOrDefault(ch, 0) - 1);
+            while (cnt == t.length()) {
+                if (r - l + 1 < minlen) {
+                    minlen = r - l + 1;
+                    sindex = l;
+                }
+                mpp.put(s.charAt(l), mpp.get(s.charAt(l)) + 1);
+                if (mpp.get(s.charAt(l)) > 0) {
+                    cnt--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return sindex == -1 ? "" : s.substring(sindex, sindex + minlen);
+    }
+}
